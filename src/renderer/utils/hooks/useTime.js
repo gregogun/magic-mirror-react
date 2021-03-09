ea6d0027d383afd809a4345d03efcebd6363f54e
Greg Ogun <gregogun97@gmail.com>
2021-03-09 23:39:29 +0000

    refactor: :recycle: change current time implementation

--- src/renderer/utils/hooks/useTime.js
+++ src/renderer/utils/hooks/useTime.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 
 const getHour = new Date().getHours();
-const hour = getHour - 12;
-const minute = new Date().getMinutes();
-const second = new Date().getSeconds();
 const date = new Date();
-const currentTime = `${hour}:${minute < 10 ? `0${minute}` : minute}`;
+const currentTime = `${
+  new Date().getHours() > 12
+    ? new Date().getHours() - 12
+    : new Date().getHours()
+}:${
+  new Date().getMinutes() < 10
+    ? `0${new Date().getMinutes()}`
+    : new Date().getMinutes()
+}`;
 
 const useTime = () => {
   const [time, setTime] = useState(currentTime);
